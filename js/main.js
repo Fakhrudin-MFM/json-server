f7c37b99a5ca731f33cb1d82b56c4d420bb2fae8
Fakhrudin-MFM <Fakhrudin.MFM@gmail.com>
2020-08-04 09:42:22 +0700

    update script: JSON.parse() pada file /js/main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET','https://fakhrudin-mfm.github.io/json-server/databases/test.json');
 ourRequest.onload = function(){
-    var ourData = ourRequest.responseText;
-    console.log(ourData[0]);
+    // var ourData = ourRequest.responseText; // data masih dalam bentuk text, di console ditampilkan "[""
+    var ourData = JSON.parse(ourRequest.responseText); // data telah dirubah dalam bentuk json, di console ditampilkan struktur json{...}"
+    console.log(ourData[0]); 
+
 };
 ourRequest.send();
