1547d15f79a1c51929623000ea6e6be66e039c0a
Fakhrudin-MFM <Fakhrudin.MFM@gmail.com>
2020-08-04 09:31:47 +0700

    update path css & javascript pada  index.html

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET','https://fakhrudin-mfm.github.io/json-server/databases/test.json');
 ourRequest.onload = function(){
     var ourData = ourRequest.responseText;
-    console.log(ourData)[0];
+    console.log(ourData[0]);
 };
 ourRequest.send();
